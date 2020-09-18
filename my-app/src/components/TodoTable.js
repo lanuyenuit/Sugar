f0f33e7378cc475add7b1abca887f9b270463d56
Uyen <lanuyen.uit@gmail.com>
2020-09-18 17:26:33 +0700

    TodoTable

--- my-app/src/components/TodoTable.js
+++ my-app/src/components/TodoTable.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/todoTable.scss";
 function TodoTable(props) {
+  const [isAddTodoDetail, setIsAddTodoDetail] = useState(false);
+  const [todoNameDetail, setTodoNameDetail] = useState("");
+  const [todoNameListDetail, setTodoNameListDetail] = useState([]);
+  let addTodoDetail = () => {
+    setIsAddTodoDetail(true);
+  };
+
+  let handleKeyDown = (e) => {
+    let list = [];
+    if (e.keyCode == 13) {
+      list = [...todoNameListDetail, todoNameDetail];
+      setTodoNameListDetail(list);
+    }
+  };
+
   return (
     <div className="todo-table">
-      <div className="title-table">
+      <div className="title-table d-flex p-2">
         <div>{props.todoName}</div>
-        <div className="d-flex">
-          <i className="fa fa-list mr-3" />
+        <div className="d-flex title-icon">
+          <i
+            className="fa fa-plus-circle mr-3"
+            onClick={() => addTodoDetail()}
+          />
           <i className="fa fa-trash" />
         </div>
+      </div>
+      <div className="body-table container-fluid">
+        {isAddTodoDetail && (
+          <input
+            type="text"
+            className="form-control mt-2"
+            placeholder="New item"
+            defaultValue={todoNameDetail}
+            onKeyDown={(e) => handleKeyDown(e)}
+            onChange={(e) => setTodoNameDetail(e.target.value)}
+          />
+        )}
+        <div>{todoNameListDetail}</div>
       </div>
     </div>
   );
