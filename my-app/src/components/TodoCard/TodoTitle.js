77f77eab144d5d505d786073c653511e15ff3df2
Uyen <lanuyen.uit@gmail.com>
2020-09-23 15:35:04 +0700

    edit title using ref

--- my-app/src/components/TodoCard/TodoTitle.js
+++ my-app/src/components/TodoCard/TodoTitle.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function TodoTitle({ title, addTodoDetail }) {
+  const [disableTitle, setDisableTitle] = useState(true);
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+  const editTodoName = () => {
+    setDisableTitle(false);
+  };
   return (
-    <div className="title-table d-flex p-2">
-      <div>{title}</div>
+    <div className="title-table d-flex p-2" onClick={() => editTodoName()}>
+      <input
+        className="titleTodoName"
+        type="text"
+        ref={inputRef}
+        disabled={disableTitle}
+        defaultValue={title}
+        onFocus={(e) => {
+          e.target.select();
+        }}
+      />
+
       <div className="d-flex title-icon">
         <i className="fa fa-plus-circle mr-3" onClick={() => addTodoDetail()} />
         <i className="fa fa-trash" />
